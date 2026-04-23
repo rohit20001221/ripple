eedc81d3bd343b916f73452aba066c87fa7f4b49
rohit20001221 <rohit20001221@gmail.com>
2026-04-23 22:27:40 +0530

    peer struct

--- peer/peer.go
+++ peer/peer.go
@@ -1,8 +1,15 @@
 package peer
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 type Peer struct {
 	IP   net.IP
 	Port uint16
+}
+
+func (p Peer) String() string {
+	return fmt.Sprintf("%s:%d", p.IP.String(), p.Port)
 }
